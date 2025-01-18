c056b58c478df01fc088ba615907886cbb1bee87
anildixit01 <anildixit01@gmail.com>
2025-01-18 09:17:18 +0530

    initial commit

--- src/test/java/com/thetestingacademy/TestSelenium.java
+++ src/test/java/com/thetestingacademy/TestSelenium.java
@@ -1,5 +1,17 @@
 package com.thetestingacademy;
 
-public class TestSeenium {
-    
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium {
+
+    @Test
+    public void test_VWOLogin(){
+
+        WebDriver driver = new EdgeDriver ();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+    }
+
 }
